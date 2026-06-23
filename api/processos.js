922f769863b52c61bbff56b2d6349dd8549a08b3
Alanacamily <alanacamily53@gmail.com>
2026-06-23 10:02:41 -0300

    Create processos.js
    adiciona api processos

---
+++ api/processos.js
@@ -0,0 +1,27 @@
+const { createClient } = require("@supabase/supabase-js");
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_KEY
+);
+
+module.exports = async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  try {
+    const { data, error } = await supabase
+      .from("processos")
+      .select("*")
+      .eq("excluido", false);
+
+    if (error) {
+      return res.status(500).json(error);
+    }
+
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message
+    });
+  }
+};
